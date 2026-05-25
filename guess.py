1aefc94bbfcb58f9c8e1c96e85fc990338555bd0
aliaghahedayat09-byte <aliaghahedayat09@gmail.com>
2026-05-25 09:22:57 -0700

    Add number guessing program to determine odd/even

---
+++ guess.py
@@ -0,0 +1,7 @@
+print("This program is going to guess the number and will tell if it is odd or over. ")
+x = int(input('Enter a number: '))
+if x%2 == 0:
+    print('even ')
+
+else:
+    print("odd")
