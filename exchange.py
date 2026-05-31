553f64c541facda5a50ddd526f5a83f21aa6bae9
aliaghahedayat09-byte <aliaghahedayat09@gmail.com>
2026-05-31 10:44:59 -0700

    Implement dollar to euro conversion in exchange.py
    Add currency conversion from dollar to euro.

---
+++ exchange.py
@@ -0,0 +1,9 @@
+# what is the value of dollor than euro 
+
+ust_amount = float(input("How much dollor do you have? "))
+rate_amount = float(input("what is rate amount? "))
+
+
+euro_amount = ust_amount * rate_amount 
+
+print("€",euro_amount)
